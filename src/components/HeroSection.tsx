4039ea1633d8e2d29457adfe74636f3c58022a8a
Carter-Bardell-Munro <cartermunro11@gmail.com>
2023-07-11 21:22:10 +1200

    feat: made hero section responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NextImage from 'next/image';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading } from '@chakra-ui/react';
 
 const imageStyle = {
     borderRadius: '3%',
@@ -9,35 +9,44 @@ const imageStyle = {
 const HeroSection = () => {
     return (
         <Flex
-            flexDir="row"
+            flexDir={{ base: 'column', lg: 'row' }}
             justifyContent="space-around"
             alignItems="center"
             py="10em"
         >
             <Box>
-                <Heading size="lg" color="white">
-                    Hi.
-                </Heading>
-                <Flex flexDir="row">
-                    <Heading size="4xl" color="#335E31">
-                        I&apos;m Carter
+                <Container
+                    maxW="md"
+                    alignItems={{ base: 'center', lg: 'space-between' }}
+                    textAlign={{ base: 'center', lg: 'start' }}
+                >
+                    <Heading size="lg" color="white">
+                        Hi.
                     </Heading>
-                    <Heading size="4xl" color="white">
-                        .
-                    </Heading>
-                </Flex>
-                <Box w={['20em', '25em', '35em']}>
-                    <Heading size="xl" color="white">
-                        A Full Stack Software Developer based out of Auckland,
-                        NZ.
-                    </Heading>
-                </Box>
+                    <Flex
+                        flexDir="row"
+                        justifyContent={{ base: 'center', lg: 'flex-start' }}
+                    >
+                        <Heading size="4xl" color="#335E31">
+                            I&apos;m Carter
+                        </Heading>
+                        <Heading size="3xl" color="white">
+                            .
+                        </Heading>
+                    </Flex>
+                    <Box>
+                        <Heading size="xl" color="white">
+                            A Full Stack Software Developer based out of
+                            Auckland, NZ.
+                        </Heading>
+                    </Box>
+                </Container>
             </Box>
             <Box>
                 <NextImage
                     src="/sanfrancisco.jpg"
                     alt="Me standing in front of wall"
-                    width={300}
+                    width={400}
                     height={100}
                     style={imageStyle}
                 />
